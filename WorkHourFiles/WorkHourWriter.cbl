8c7cffdb9ad2ed13c5da4a3a5e51d8583364fba1
Andreas Richardsen <andreas.w.richardsen@gmail.com>
2025-11-02 22:58:38 +0100

    Added standard days for easier inputting

--- WorkHourFiles/WorkHourWriter.cbl
+++ WorkHourFiles/WorkHourWriter.cbl
@@ -27,6 +27,7 @@
 
        WORKING-STORAGE SECTION.
        01 LineBreak             PIC X(72) VALUE ALL "-".
+       01 TypeOfDay             PIC X.
        01 LastEntry.
            02 LEDate.
               03 LEYear         PIC 9(4).
@@ -42,6 +43,16 @@
               03 LEEndHour      PIC 9(2).
               03 FILLER         PIC X       VALUE ":".
               03 LEEndMinute    PIC 9(2).
+       01 StandardHours.
+           02 SHStart1          PIC 9(4)    VALUE 0730.
+           02 SHEnd1           PIC 9(4)     VALUE 1150.
+           02 SHStart2          PIC 9(4)    VALUE 1220.
+           02 SHEnd2            PIC 9(4)    VALUE 1600.
+       01 StandarHoursFriday.
+           02 SHFStart1         PIC 9(4)    VALUE 0800.
+           02 SHFEnd1           PIC 9(4)    VALUE 1150.
+           02 SHFStart2         PIC 9(4)    VALUE 1220.
+           02 SHFEnd2           PIC 9(4)    VALUE 1400.
 
        PROCEDURE DIVISION.
 
@@ -64,19 +75,20 @@
               IF WHDate = SPACE 
                  EXIT PERFORM
               END-IF 
-              DISPLAY "Start time (HHMM)"
-              ACCEPT WHStartTime
-              DISPLAY "End time (HHMM)"
-              ACCEPT WHEndTime
-              COMPUTE WHHoursWorked ROUNDED = 
-                 (((WHEndHour * 60) + WHEndMinute) 
-                 - ((WHStartHour * 60) + WHStartMinute))/60
+              DISPLAY "Enter = Non standard day"
+              DISPLAY "s = Standard day"
+              DISPLAY "f = Standard friday"
+              ACCEPT TypeOfDay 
+              EVALUATE TypeOfDay 
+                 WHEN SPACE  PERFORM NonStandardDay
+                 WHEN "s"    PERFORM StandardDay
+                 WHEN "f"    PERFORM StandardFriday
+              END-EVALUATE
               WRITE WorkHourDetails
               PERFORM MoveToLastEntry
               END-PERFORM
            CLOSE WorkHourFile           
            STOP RUN.
-      *    Test     
 
        MoveToLastEntry.
               MOVE WHYear TO LEYear 
@@ -86,4 +98,39 @@
               MOVE WHStartMinute TO LEStartMinute 
               MOVE WHEndHour TO LEEndHour 
               MOVE WHEndMinute TO LEEndMinute.
+
+       StandardDay.
+              MOVE SHStart1 TO WHStartTime 
+              MOVE SHEnd1 TO WHEndTime 
+              COMPUTE WHHoursWorked ROUNDED = 
+                 (((WHEndHour * 60) + WHEndMinute) 
+                 - ((WHStartHour * 60) + WHStartMinute))/60
+              WRITE WorkHourDetails
+              MOVE SHStart2 TO WHStartTime 
+              MOVE SHEnd2 TO WHEndTime 
+              COMPUTE WHHoursWorked ROUNDED = 
+                 (((WHEndHour * 60) + WHEndMinute) 
+                 - ((WHStartHour * 60) + WHStartMinute))/60.
+
+       StandardFriday.
+              MOVE SHFStart1 TO WHStartTime 
+              MOVE SHFEnd1 TO WHEndTime 
+              COMPUTE WHHoursWorked ROUNDED = 
+                 (((WHEndHour * 60) + WHEndMinute) 
+                 - ((WHStartHour * 60) + WHStartMinute))/60
+              WRITE WorkHourDetails
+              MOVE SHFStart2 TO WHStartTime 
+              MOVE SHFEnd2 TO WHEndTime 
+              COMPUTE WHHoursWorked ROUNDED = 
+                 (((WHEndHour * 60) + WHEndMinute) 
+                 - ((WHStartHour * 60) + WHStartMinute))/60.
+
+       NonStandardDay.
+              DISPLAY "Start time (HHMM)"
+              ACCEPT WHStartTime
+              DISPLAY "End time (HHMM)"
+              ACCEPT WHEndTime
+              COMPUTE WHHoursWorked ROUNDED = 
+                 (((WHEndHour * 60) + WHEndMinute) 
+                 - ((WHStartHour * 60) + WHStartMinute))/60.
            
